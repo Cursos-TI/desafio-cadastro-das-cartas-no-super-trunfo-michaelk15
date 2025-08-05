b89dc50a2767aaf61ab68eb55d33adf6099180a9
MICHAEL AMARAL KAWAMURA <michaelads1@icloud.com>
2025-08-05 13:16:50 +0000

    colocado todos os dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
-int main(){
-    printf("DESAFIO SUPER TRUNFO!\n");
+int main() {
+    // Carta 1 - São Paulo
+    char estado1 = 'A';
+    char codigo1[] = "A01";
+    char nomeCidade1[] = "São Paulo";
+    int populacao1 = 12325000;
+    float area1 = 1521.11;
+    float pib1 = 699.28;
+    int pontosTuristicos1 = 50;
 
+    // Carta 2 - Rio de Janeiro
+    char estado2 = 'B';
+    char codigo2[] = "B02";
+    char nomeCidade2[] = "Rio de Janeiro";
+    int populacao2 = 6748000;
+    float area2 = 1200.25;
+    float pib2 = 300.50;
+    int pontosTuristicos2 = 30;
+
+  
+
+    return 0;
 }
+    
